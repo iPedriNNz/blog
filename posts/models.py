d4641ade3212c12429e13a416ce16b683316bf8c
Pedro <iskol.gameplay@gmail.com>
2022-03-29 20:13:25 -0300

    Adicionado redimensinamento de imagem

--- posts/models.py
+++ posts/models.py
@@ -24,20 +24,20 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
         if self.imagem_post:
-            self.resize_img(self.imagem_post.name, 800)
+            self.resize_image(self.imagem_post.name, 800)
 
     @staticmethod
-    def resize_img(img_name, new_width):
+    def resize_image(img_name, new_width):
         img_path = os.path.join(settings.MEDIA_ROOT, img_name)
         img = Image.open(img_path)
         width, height = img.size
-        new_height = round((new_width * height / width))
+        new_height = round((new_width * height) / width)
 
         if width <= new_width:
             img.close()
             return
 
-        new_img = img.resize((new_height, new_width), Image.ANTIALIAS)
+        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
         new_img.save(
             img_path,
             optimize=True,
